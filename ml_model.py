83e9ccb9caa4f1e0061f5392bce79a88b70997c3
DineshKambham7 <dinesh7.learning@gmail.com>
2022-03-27 15:39:01 +0530

    Added 3 new ML models

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code to ML model1")
+
+def model2():
+    print("Adding code to ML model2")
+
+def model3():
+    print("Adding code to ML model3")
